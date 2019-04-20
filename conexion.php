45675191d0cb4ea27c496d6da1634a61b694e44a
saniag081 <sgonzalezhernandez081@gmail.com>
2019-04-19 22:58:17 -0500

    primer commit

---
+++ conexion.php
@@ -0,0 +1,22 @@
+<?php 
+
+    $link = 'mysql:host=localhost;dbname=colores';
+    $usuario = 'root';
+    $pass = '';
+
+    try{
+        //conectarse a la base de datos
+        $mbd = new PDO ($link,$usuario,$pass);
+
+        echo 'conectado';
+        
+        foreach($mbd->query('SELECT * FROM  `color` ') as $fila) {
+            print_r($fila);
+        }
+    } catch (PDOException $e) { //funcion de PDO error en pantalla
+        print "¡Error!: " . $e->getMessage() . "<br/>";
+        die();
+    }
+
+
+?>
